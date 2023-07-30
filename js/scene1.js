60cf2f3dc905873bd0a7c312b99319aacdcc2961
Wendi Fu <Wd_fu@outlook.com>
2023-07-30 12:27:08 -0500

    working on js3

--- js/scene1.js
+++ js/scene1.js
@@ -13,7 +13,6 @@ var svg = d3.select("#scene1")
  
 d3.csv("https://kennn98.github.io/data/human-development-index-hdi-2014.csv")
 .then( function(data, i) {
-    d3.select('#scene1').attr("width", 500).attr("height", 400)
 
     var x_HDI = d3.scaleLinear()
     .domain([0, 1])
@@ -21,14 +20,17 @@ d3.csv("https://kennn98.github.io/data/human-development-index-hdi-2014.csv")
     svg.append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x_HDI));
+
     var y_GNI = d3.scaleLinear()
     .domain([0, 90000])
     .range([ height, 0]);
     svg.append("g")
     .call(d3.axisLeft(y_GNI));
+    
     var z_GNI = d3.scaleLinear()
     .domain([0, 90000])
     .range([2, 20]);
+    
     var colorScale = d3.interpolateHclLong("red","green")
 
     svg.append('g')
@@ -38,7 +40,7 @@ d3.csv("https://kennn98.github.io/data/human-development-index-hdi-2014.csv")
     .append("circle")
     .attr("cx", function (d, i) { return x_HDI(parseFloat(d.HDI)); } )
     .attr("cy", function (d, i) { return y_GNI(parseInt(d.GNI)); } )
-    .attr("r", function (d, i) {return z_GNI(parseInt(d.GNI))})
+    .attr("r", 5)
     .attr("fill",function (d, i) {return colorScale(parseFloat(d.HDI))})
     .attr("opacity", 0.8)
 
@@ -47,7 +49,7 @@ d3.csv("https://kennn98.github.io/data/human-development-index-hdi-2014.csv")
     .append('text')
     .attr('text-anchor', 'middle')
     .attr('transform', 'rotate(-90)')
-    .text('GNI')
+    .text('GNI(usd)')
 
     svg.append('g')
     .attr('transform', 'translate(' + 380 + ', ' + 360 + ')')
---
+++ js/scene2.js
@@ -0,0 +1,63 @@
+var margin = {top: 10, right: 10, bottom: 90, left: 45},
+width = 1500 - margin.left - margin.right,
+height = 500 - margin.top - margin.bottom;
+
+var svg = d3.select("#scene2")
+.append("svg")
+.attr("width", width + margin.left + margin.right)
+.attr("height", height + margin.top + margin.bottom)
+.append("g")
+.attr("transform",
+      "translate("+margin.left+","+margin.top+")");
+
+// Parse the Data
+d3.csv("https://kennn98.github.io/data/human-development-index-hdi-2014.csv").then( function(data, i) {
+
+var x_Country = d3.scaleBand()
+    .range([ 0, width ])
+    .domain(data.map(function(d) { return d.Country; }))
+    .padding(0.03);
+    svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x_Country))
+    .selectAll("text")
+    .attr("transform", "translate(-10,0)rotate(-75)")
+    .style("text-anchor", "end");
+
+var y_GNI = d3.scaleLinear()
+    .domain([0, 90000])
+    .range([0, height]);
+    svg.append("g")
+    .call(d3.axisLeft(y_GNI)
+    .tickValues([0, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000])
+    .tickFormat((d, i) => 
+        ['90000', '80000', '70000', '60000', '50000', '40000', '30000', '20000', '10000', '0'][i]));
+
+var colorScale = d3.interpolateHclLong("red","green")
+
+// Bars
+svg.selectAll("barchart")
+.data(data)
+.enter()
+.append("rect")
+.attr("x", function(d) { return x_Country(d.Country); })
+.attr("width", x_Country.bandwidth())
+.attr("y", function(d) { return height - y_GNI(d.GNI); })
+.attr("fill", function (d, i) {return colorScale(parseFloat(d.HDI))})
+.attr("height", function(d) { return y_GNI(d.GNI) ; })
+
+svg.append('g')
+    .attr('transform', 'translate(' + 20 + ', ' + 50 + ')')
+    .append('text')
+    .attr('text-anchor', 'middle')
+    .attr('transform', 'rotate(-90)')
+    .text('GNI(usd)')
+
+const x_labelheight = height - 10;
+const x_labelwidth = width - 20;
+svg.append('g')
+    .attr('transform', 'translate(' + x_labelwidth + ', ' + x_labelheight + ')')
+    .append('text')
+    .attr('text-anchor', 'middle')
+    .text('Country')
+})
